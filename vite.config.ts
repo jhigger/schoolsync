7b11e4edf7fc1aa75d89d64d0b5fbf8d7bd95b54
jhigger <30231759+jhigger@users.noreply.github.com>
2026-07-18 06:14:10 +0000

    chore: revert build to vite and remove nitro

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,10 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), nitro({}), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(),  tanstackStart(), viteReact()],
   test: {
     globals: true,
     environment: 'jsdom',
